d4e7c9deb082c1d20988714d7be0eacc084e6dbc
Darrion Gering and Kaya Jepson <kaya@>
2019-03-27 13:49:43 -0700

    add functionality to calculator

--- js/scripts.js
+++ js/scripts.js
@@ -19,10 +19,30 @@ var divide = function(number1, number2) {
 // Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $(".add-button").click(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     console.log(add(number1, number2));
+
+    $(".subtract-button").click(function(event) {
+      event.preventDefault();
+      var number1 = parseInt($("#add1").val());
+      var number2 = parseInt($("#add2").val());
+      console.log(subtract(number1, number2));
+    });
+    $(".multiply-button").click(function(event) {
+      event.preventDefault();
+      var number1 = parseInt($("#add1").val());
+      var number2 = parseInt($("#add2").val());
+      console.log(multiply(number1, number2));
+    });
+    $(".divide-button").click(function(event) {
+      event.preventDefault();
+      var number1 = parseInt($("#add1").val());
+      var number2 = parseInt($("#add2").val());
+      console.log(divide(number1, number2));
+    });
   });
+
 });
